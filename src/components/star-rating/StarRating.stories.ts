70e4113eb1737ed21f39593e72245806327e18e3
Jan Rode <jan.rode@sybit.de>
2020-06-29 15:47:45 +0200

    fix typo

--- src/components/star-rating/StarRating.stories.ts
+++ src/components/star-rating/StarRating.stories.ts
@@ -4,7 +4,7 @@ import { number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 export default {
-  title: 'Start Rating',
+  title: 'Star Rating',
   decorators: [setupSpartacus([StarRatingModule])],
 };
 
